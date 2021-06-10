90e04aa2e82d9929076a54bd5f4e6327442ad532
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-06-10 19:06:46 +0530

    added routing

---
+++ src/pages/checkout.js
@@ -0,0 +1,18 @@
+import Header from "../components/Header"
+
+const Checkout = () => {
+    return (
+        <div className="bg-gray-100">
+            <Header />
+
+            <main className="lg:flex max-w-2xl mx-auto">
+                {/* left  */}
+
+                {/* right */}
+            </main>
+
+        </div>
+    )
+}
+
+export default Checkout
