0667de949dd6a36939dba5bcff06b31127d743d9
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-06-11 23:20:14 +0530

    fixed ui bug

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -15,7 +15,7 @@ const Checkout = () => {
         <div className="bg-gray-100">
             <Header />
 
-            <main className="lg:flex max-w-2xl mx-auto">
+            <main className="lg:flex max-w-screen-2xl mx-auto">
                 {/* left  */}
                 <div className="flex-grow m-5 shadow-sm">
                     <Image
